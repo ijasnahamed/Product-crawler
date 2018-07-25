c8094a61c3a0b0e7168e4f996d14217c70bba971
Ijas Ahamed N <ijasnahamed@gmail.com>
2018-07-25 15:56:58 +0530

    Get product name after crawling product url

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var product_crawler = require('./product_crawler');
 var url = 'https://www.flipkart.com/mi-casual-19-l-laptop-backpack/p/itmf4upfnnghex79?pid=BKPF4UPFYMDUNHTG&lid=LSTBKPF4UPFYMDUNHTGYC0RE8&marketplace=FLIPKART&srno=b_1_1&otracker=hp_omu_Fashion%20Accessories_2_Only%20on%20Flipkart_FRUY012J2JIS_0&fm=neo/merchandising&iid=62d31f77-640b-471d-814a-3c1c0d08fa0c.BKPF4UPFYMDUNHTG.SEARCH&ppt=Homepage&ppn=Homepage&ssid=dtu771l5680000001532164309746';
 // var url = 'https://www.flipkart.com/mi-travel-18-l-laptop-backpack/p/itmf4upfw6gsaryy?pid=BKPF4UPFUJKWNEJB&lid=LSTBKPF4UPFUJKWNEJB8ZO5L6&marketplace=FLIPKART&srno=b_1_2&otracker=hp_omu_Fashion%20Accessories_2_Only%20on%20Flipkart_FRUY012J2JIS_0&fm=neo/merchandising&iid=ff3a903f-3eb1-4a80-ad9f-67d2a33473db.BKPF4UPFUJKWNEJB.SEARCH&ppt=Homepage&ppn=Homepage&ssid=dtu771l5680000001532164309746';
 
-product_crawler.get_product_name(url, function(err, res) {
+product_crawler.get_name(url, function(err, name) {
 	if(err) {
-		console.log('Error: '+err);
-	} else {
-
-		console.log('product name is '+res);
+		console.log('Error: '+err.message);
+		return;
 	}
+	console.log('product name is '+name);
+
 });
--- product_crawler.js
+++ product_crawler.js
@@ -1,25 +1,54 @@
 var Crawler = require("crawler");
 var c = new Crawler();
 
-var get_product_name = function(url, callback) {
+var ProductCrawler = function(url) {
+    this.url = url;
+    this.name = "";
+};
+
+ProductCrawler.prototype.crawl = function() {
+    return new Promise(function(resolve, reject) {
+        c.queue([{
+            uri: his.url,
+            callback: function (error, res, done) {
+                if(error) {
+
+                }
+                var $ = res.$;
+                var title = $("title").text();
+                title = title.split(/[^a-zA-Z0-9\s']+/);
+                if(title.length > 0) {
+                    console.log("page crawling successful. Product name: "+title[0]);
+                    callback(null, title[0]);
+                } else {
+                    callback(new Error('Failed to fetch product name'), null);
+                }            
+                done();
+            }
+        }]);
+    });
+};
+
+var get_name = function(url, callback) {
     console.log("Crawling "+url);
-    c.queue([{
-        uri: url,
-        callback: function (error, res, done) {
-            var $ = res.$;
-            var title = $("title").text();
-            title = title.split(/[^a-zA-Z0-9\s']+/);
-            if(title.length > 0) {
-                console.log("page crawling successful. Product name: "+title[0]);
-                callback(null, title[0]);
-            } else {
-                callback('Failed to fetch product name', null);
-            }            
-            done();
-        }
-    }]);
+        c.queue([{
+            uri: url,
+            callback: function (error, res, done) {
+                var $ = res.$;
+                var title = $("title").text();
+                title = title.split(/[^a-zA-Z0-9\s']+/);  
+                console.log("success");     
+                done();
+                if(title.length > 0) {
+                    console.log("page crawling successful. Product name: "+title[0]);
+                    callback(null, title[0]);
+                } else {
+                    callback(new Error('Failed to fetch product name'), null);
+                }     
+            }
+        }]);
 };
 
 module.exports = {
-    get_product_name: get_product_name
+    get_name: get_name
 };
